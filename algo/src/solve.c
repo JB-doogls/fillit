bc8c61146c874e137bee87bbd531a978a6b983f8
J.B.Doogls <jbdoogls@edoll.21vv>
2019-11-01 12:15:14 +0300

    functions: control, make_map, check_spaces, put_shape + backtracking(prototype)

--- algo/src/solve.c
+++ algo/src/solve.c
@@ -6,13 +6,17 @@
 /*   By: jbdoogls <jbdoogls@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 14:49:10 by edoll             #+#    #+#             */
-/*   Updated: 2019/11/01 11:04:41 by jbdoogls         ###   ########.fr       */
+/*   Updated: 2019/11/01 12:14:32 by jbdoogls         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
 
-int ft_solving(t_fil *list)
+/*
+**  solving algorithm control module
+*/
+
+int     ft_solving(t_tetri *list)
 {
     char **map;
     int j;
@@ -23,11 +27,10 @@ int ft_solving(t_fil *list)
     if (!(map = ft_make_map(&map, map_size))
         return (-1);
     while (!(ft_backtrack_algo(map, list, map_size))
-        if (!(map = ft_make_map(&map, map_size++))
+        if (!(map = ft_make_map(map, map_size++))
             retrun (-1);
     
-    
-    j = 0;                          // <-- seems we can impliment the func to printing result here //
+    j = 0;                          // <-- it seems we can impliment the uotput function here //
     while (map[j])                
     {
         ft_putstr(map[j]);
@@ -36,8 +39,11 @@ int ft_solving(t_fil *list)
     return (0);
 }
 
+/*
+**  function to create and increase the map for arrange shapes
+*/
 
-char    **ft_make_map(char **map, map_size)
+char    **ft_make_map(char **map, int map_size)
 {
     int x;
     int y;
@@ -52,7 +58,7 @@ char    **ft_make_map(char **map, map_size)
         return (NULL);
     *map[map_size] == NULL;
     y = 0;
-    while (y < z)
+    while (y < map_size)
     {
         if (!(*map[y] = (char*)malloc(sizeof(char) * (map_size + 2))))
         {
@@ -61,8 +67,8 @@ char    **ft_make_map(char **map, map_size)
             return (NULL);
         }
         x = 0;
-        map[y][z] = '\n';
-        map[y][z + 1] = '\0';
+        map[y][map_size] = '\n';
+        map[y][map_size + 1] = '\0';
         while (x < map_size)
             map[y][x++] = '.';
         y++;
@@ -70,35 +76,97 @@ char    **ft_make_map(char **map, map_size)
     return (*map);
 }
 
-char **ft_backtrack_algo(map, t_fil *list, map_size)
+/*
+**  recursive algorithm for solving the issue 
+**      this is prototype - without a tool for delete incorrect turn of shape puting and nessesuary return-values the same     
+*/
+
+char    **ft_backtrack_algo(char **map, t_tetri *list, int map_size)
 {
     int x;
     int y;
     
     y = 0;
-    while (y < z)
+    while (y++ < map_size)
     {
         x = 0;
-        while (x < z)
+        while (x++ < map_size)
         {
-            if (ft_check_space_for_shape(list, x, y, map_size))
+            if (ft_check_space_for_shape(list, map, x, y, map_size))
             {
-                // ft_put_shape(.....);                                        // ADD SHAPE_LETTER IN PARAMS !!! //
+                ft_put_shape(list, map);
                 if (list->next && ft_backtrack_algo(map, list, map_size))
                     return (map);
                 if (!list->next)
-                    ft_put_shape(......);
+                {
+                    ft_put_shape(list, map);
+                    return (map);
+                }
                 else
                 {
-                    ft_del_shape
+                    // mark an error-flag ??? \ return error ???
+                    // ft_del_shape
                 }
                 
                     
             }
             x++;
         }
+        y++;
     }
     return (NULL);
 }
 
-int ft_check_space_for_shape(t_fil *list,)
+/*
+**  check map from current specific point (x,y) on possibility to put the shape following it's coordinates in t_tetri
+**  the first condition is about to validate the directly point we stand in
+*/
+
+int     ft_check_space_for_shape(t_tetri *lt, char **map, int x, int y, int map_size)
+{
+    if (map[y][x] == '.' &&
+    map[y + lt->shape[0]][x + lt->shape[1]] == '.' &&
+    map[y + lt->shape[0]][x + lt->shape[1]] < map_size &&
+    map[y + lt->shape[2]][x + lt->shape[3]] == '.' &&
+    map[y + lt->shape[2]][x + lt->shape[3]] < map_size &&
+    map[y + lt->shape[4]][x + lt->shape[5]] == '.' &&
+    map[y + lt->shape[4]][x + lt->shape[5]] < map_size &&
+    map[y + lt->shape[6]][x + lt->shape[7]] == '.' &&
+    map[y + lt->shape[6]][x + lt->shape[7]] < map_size)
+        return (1);
+    return (0);
+}
+
+/*
+**  here is small function to put she shape following it's coordinate after succes all checkings
+*/
+
+void    ft_put_shape(t_tetri *lt, char **map)
+{
+    map[y + lt->shape[0]][x + lt->shape[1]] = lt->c;
+    map[y + lt->shape[2]][x + lt->shape[3]] = lt->c;
+    map[y + lt->shape[4]][x + lt->shape[5]] = lt->c;
+    map[y + lt->shape[6]][x + lt->shape[7]] = lt->c;
+}
+
+
+
+
+
+
+
+/*
+** **********************************************************
+*//*
+
+shape[8] = {0,0, 0,1  1,0, 1,1}
+            y,x  y,x  y,x  y,x
+            0 1  2 3  4 5  6 7
+
+typedef struct		s_tetri
+{
+	int					shape[8];
+	int					c;
+	struct s_tetri		*next;
+}					t_tetri;
+*/
