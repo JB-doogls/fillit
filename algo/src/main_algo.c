e607c0b5aaa232d9c24d6ff2a7d34cb64abaec36
J.B.Doogls <jbdoogls@edoll.21vv>
2019-11-03 05:56:40 +0300

    main with correct setting test_tetri

--- algo/src/main_algo.c
+++ algo/src/main_algo.c
@@ -2,7 +2,6 @@
 
 #include "../includes/fillit.h"
 
-
 t_tetri	*lst_new_set()
 {
 	t_tetri *new;
@@ -10,64 +9,91 @@ t_tetri	*lst_new_set()
 
 	if (!(new = (t_tetri*)malloc(sizeof(t_tetri) * 1)))
 		return (NULL);
-	new->c = '0';
+	new->letter = '0';
 	while (i < 8)
-	new->shape[i++] = 0 + i;
+		new->shape[i++] = -1;
 	new->next = NULL;
 	return (new);
 }
 
-t_tetri		*set_test(t_tetri *node)
-{
-	t_tetri *head;
-	t_tetri *tmp;
-	int i = 0;
-	int j = 0;
-	int ct = 4;
-	
-	int	kvadrat[8] = {0, 0, 0, 1, 1, 0, 1, 1};
-	int palka_vniz[8] = {0, 0, 1, 0, 2, 0, 3, 0};
-	int palka_vbok[8] = {0, 0, 0, 1, 0, 2, 0, 3};
-	int loshadyu_hodi_vpravo[8] = {0, 0, 0, 1, 1, 0, 2, 0};
-
-	head = node;
-	if (!node->next && ct--)
-	{
-		if (!(tmp = lst_new_set()))
-			return (NULL);
-		node->next = tmp;
-	}
-
-	node->shape[i++] = kvadrat[j++];
-	node = node->next;
-	i = j = 0;
-	printf("%d\n%d\n", i, j);
-	node->shape[i++] = palka_vniz[j++];
-	node = node->next;
-	i = j = 0;
-	node->shape[i++] = palka_vbok[j++];
-	node = node->next;
-	i = j = 0;
-	node->shape[i++] = loshadyu_hodi_vpravo[j++];
-	node = node->next;
-	
-	return (head);
-}
 
 int 	main()
 {
 	t_tetri *node;
-	
+	t_tetri *head;
+	int test_shapes[4][8] = {
+		{0, 0, 0, 1, 1, 0, 1, 1}, {0, 0, 1, 0, 2, 0, 3, 0},
+		{0, 0, 0, 1, 0, 2, 0, 3}, {0, 0, 0, 1, 1, 0, 2, 0}};
+	int i = 0;
+	int j = 0;
+
 	if (!(node = lst_new_set()))
-		return (-1);
-	set_test(node);
-	if (ft_solving(node) == -1)
 	{
-		printf("Error\n");
-		return (0);
+		printf("Error creating list node #" "%d\n", j);
+		return (-1);
+	}
+	head = node;
+	printf("content node #" "%d\n", j);
+	while (i < 8)
+	{
+		node->shape[i] = test_shapes[j][i];
+		i++;
+	}
+	printf("\tnode->shape = {""%d,%d,%d,%d,%d,%d,%d,%d" "}\n", node->shape[0],
+	node->shape[1], node->shape[2], node->shape[3], node->shape[4], node->shape[5],
+	node->shape[6], node->shape[7]);
+
+	node->letter = 'A' + j;
+	printf("\tnode->letter = ""%c\n", node->letter);
+	j += 1;
+	while (j < 4)
+	{
+		if (!(node->next = lst_new_set()))
+		{
+			printf("Error creating list node #" "%d\n", j);
+			return (-1);
+		}
+		node = node->next;
+		i = 0;
+		printf("\ncontent node #" "%d\n", j);
+		while (i < 8)
+		{
+			node->shape[i] = test_shapes[j][i];
+			i++;
+		}
+		printf("\tnode->shape = {""%d,%d,%d,%d,%d,%d,%d,%d" "}\n", node->shape[0],
+		node->shape[1], node->shape[2], node->shape[3], node->shape[4], node->shape[5],
+		node->shape[6], node->shape[7]);
+		
+		node->letter = 'A' + j;
+		printf("\tnode->letter = ""%c\n", node->letter);
+		j++;
+	}
+	if (!(head->letter == 'A' + 0))
+	{
+		printf("incorrect *head\n");
+		return (-1);
+	}
+	printf("\ncheck head node\n");
+	printf("\thead->shape = {""%d,%d,%d,%d,%d,%d,%d,%d" "}\n", head->shape[0],
+	head->shape[1], head->shape[2], head->shape[3], head->shape[4], head->shape[5],
+	head->shape[6], head->shape[7]);
+	printf("\thead->letter = ""%c\n", head->letter);
+	if ((head->next))
+	{
+		printf("\t->next(1)->letter = ""%c\n", (head->next)->letter);
+		printf("\t->next(2)->letter = ""%c\n", (head->next->next)->letter);
+		printf("\t->next(3)->letter = ""%c\n", (head->next->next->next)->letter);
 	}
 	return (0);
 }
+
+
+// 	if (ft_solving(node) == -1)
+// 	{
+// 		printf("Error solvation\n");
+// 		return (-1);
+// 	}
 
 
 
@@ -86,4 +112,3 @@ int 	main()
 	// xx		0, 0, 0, 1, 1, 0, 2, 0
 	// x
 	// x
-
